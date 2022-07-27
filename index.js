f2f0718c4f691dfc175d81a5cbdd63ae3d058a1d
sp-tam1m <white.hat.tamim@gmail.com>
2022-07-27 14:28:35 +0600

    created user collection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
@@ -20,6 +21,7 @@ async function run() {
         await client.connect();
         const CycleProducts = client.db('cycledb').collection('cycleproducts');
         const productsOrder = client.db('cycledb').collection('productsorder');
+        const users = client.db('cycledb').collection('users');
         console.log("CycleDB Connected")
 
         // get Cycle Products
@@ -43,6 +45,21 @@ async function run() {
             const body = req.body;
             const orders = await productsOrder.insertOne(body);
             res.send(orders)
+        })
+
+
+        // Create users
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: user,
+            }
+            const result = await users.updateOne(filter, updatedDoc, options);
+            const token = jwt.sign({ email: email }, process.env.TOKEN_SECRET, { expiresIn: '2h' })
+            res.send({ result, token })
         })
 
 
